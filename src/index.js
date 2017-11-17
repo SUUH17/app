0b11c0b442a61652bb017ce77b10580608c493dc
Jere Vaara <jerevaara@gmail.com>
2017-11-17 19:38:57 +0200

    redux router links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import Link from 'redux-first-router-link'
 
 import store from './store'
 
 const App = () =>
   <div>
     Main app moi siisti juttu
+    
+    <Link to='/items/53412'>test</Link>
+    <Link to='/'>home</Link>
   </div>
 
 ReactDOM.render(
