5cd3b317c1aeabe2e33bbcd4a0c51c930a6a0d4b
Jere Vaara <jerevaara@gmail.com>
2017-11-17 19:09:55 +0200

    frontend build thingies

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () =>
+  <div>
+    Main app moi siisti juttu
+  </div>
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
