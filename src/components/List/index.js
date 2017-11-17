752f4e746d627ed7aaaf0c486fa7a007e6d149fa
Jere Vaara <jerevaara@gmail.com>
2017-11-17 22:07:50 +0200

    css grid magix

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,12 +6,15 @@ import styles from './list.scss'
 const Item = (props) =>
   <div className={styles.item}>
     Item
+    { props.children }
   </div>
 
 const List = () =>
   <div className={styles.list}>
     { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(i => 
-      <Item key={i} props={{data: 'dummy'}} />
+      <Item key={i} props={{data: 'dummy'}}>
+       <div>index: { i }</div>
+      </Item>
     ) }
   </div>
 
