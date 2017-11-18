4ea968e472aa704a6cde32056e488a8b8a1bdad8
Jere Vaara <jerevaara@gmail.com>
2017-11-18 15:11:15 +0200

    clear search button and style things

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { X as CrossIcon } from 'react-feather'
 
 import { setSearch } from './../../store/actions'
 
@@ -8,6 +9,10 @@ import style from './search.scss'
 const Search = ({ className, searchString, setSearchString }) =>
   <div className={style.searchInput + ' ' + className}>
     <input type="text" placeholder="search..." value={searchString} onChange={e => setSearchString(e.target.value)} />
+    <CrossIcon 
+      className={style.cross} 
+      style={{ opacity: !searchString ? 0 : 1 }}
+      onClick={() => setSearchString('')}/>
   </div>
 
 const mapStateToProps = state => ({
