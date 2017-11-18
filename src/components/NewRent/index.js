f26fae1399577ac9df94a7cd20559e7267ed2189
Jere Vaara <jerevaara@gmail.com>
2017-11-18 19:01:48 +0200

    form fix

--- src/components/NewRent/index.js
+++ src/components/NewRent/index.js
@@ -5,22 +5,24 @@ import { uploadRental } from './../../store/actions'
 
 import styles from './newRent.scss'
 
-const Field = ({ field, label, type, handler }) =>
+const Field = ({ field, label, type, handler, state }) =>
   <div className={styles.formField}>
     <span>{label}</span>
-    <input type={type} onChange={handler(field)} />
+    <input type={type} onChange={handler(field)} value={state} />
   </div>
 
 class NewRent extends React.Component {
   constructor() {
     super()
     this.state = {
-      name: undefined,
-      location: undefined,
+      name: '',
+      location: '',
       price: 0,
       collateral: 0
     }
   }
+  getValue = (key) =>
+    this.state[key]
   handleInput = (key) =>
     event =>
       this.setState({
@@ -31,6 +33,15 @@ class NewRent extends React.Component {
     if (this.imagePreview && files[0]) {
       this.imagePreview.setAttribute('src', window.URL.createObjectURL(files[0]))
     }
+  }
+  clearForm = () => {
+    console.log('clearform')
+    this.setState({
+      name: '',
+      location: '',
+      price: 0,
+      collateral: 0
+    })
   }
   submitForm = () => {
     const rentalData = this.state
@@ -47,26 +58,27 @@ class NewRent extends React.Component {
   canSubmit = () => {
     const { name, location, price, collateral } = this.state
     const files = this.imageSelector && this.imageSelector.files.length >= 0
-    return 
-      files
+    return
+    files
       && !!name
       && !!location
       && !!price
       && !!collateral
   }
   render () {
+    const { name, location, price, collateral } = this.state
     return (
       <div className={styles.container}>
         <h2>Create new rental</h2>
         <div className={styles.rentalForm}>
 
-          <Field field="name" type="text" label="Item name" handler={this.handleInput} />
+          <Field field="name" type="text" label="Item name" handler={this.handleInput} state={name} />
 
-          <Field field="location" type="text" label="Address" handler={this.handleInput} />
+          <Field field="location" type="text" label="Address" handler={this.handleInput} state={location} />
 
-          <Field field="price" type="number" label="Price per hour" handler={this.handleInput} />
+          <Field field="price" type="number" label="Price per hour" handler={this.handleInput} state={price} />
 
-          <Field field="collateral" type="number" label="Collateral" handler={this.handleInput} />
+          <Field field="collateral" type="number" label="Collateral" handler={this.handleInput} state={collateral} />
 
           <div className={styles.formField}>
             <span>Image</span>
@@ -85,6 +97,7 @@ class NewRent extends React.Component {
           <div className={styles.buttons}>
             <button
               className={styles.cancelButton}
+              onClick={this.clearForm}
             >
               Cancel
             </button>
