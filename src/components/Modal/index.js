fd9ecf63472221118434fdde5447c99aaf512840
Jere Vaara <jerevaara@gmail.com>
2017-11-18 21:30:32 +0200

    rent modal stuff

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,36 +5,68 @@ import classNames from 'classnames'
 
 import Login from './../Login'
 import { dismissModal } from '../../store/actions'
+import {  selectItem } from '../../store/selectors'
 
 import style from './modal.scss'
 
-const Modal = ({ dismiss, content, location }) =>
-  <div className={style.modal}>
+const LoginModal = ({ dismiss }) =>
+  <div>
     <div className={style.modalHeader}>
       <ArrowLeft onClick={dismiss} />
     </div>
     <div className={style.modalContent}>
-      { location === 'SHOW_LOGIN' && <Login /> }
+      <Login />
     </div>
   </div>
 
-const ModalContainer = ({ visible, modalContent, dismiss, location }) =>
+const ItemModal = ({ data, dismiss }) =>
+  <div>
+    <div className={style.modalHeader}>
+      <ArrowLeft onClick={dismiss} />
+    </div>
+    <div className={style.modalContent}>
+      <div
+        className={style.imageContainer}
+        style={{
+          backgroundImage: `url(${API_BASE}images/${data.imageId})`
+        }}
+      >
+      </div>
+      <div className={style.infoContainer}>
+        <div className={style.infoHeader}>
+          <h2>{data.name}</h2>
+          <span className={style.price}>{data.price + ' €/h'}</span>
+        </div>
+        <span>{data.location}</span>
+      </div>
+    </div>
+  </div>
+
+const mapItemStateToProps = (state, props) => ({
+  data: selectItem(state, props)
+})
+
+const RentalItemModal = connect(mapItemStateToProps)(ItemModal)
+
+const ModalContainer = ({ visible, itemId, dismiss, location }) =>
   visible
     ? <div className={style.modalContainer}>
       <div
         className={style.backdrop}
         onClick={dismiss}
       ></div>
-      <Modal content={modalContent} dismiss={dismiss} location={location} />
+      <div className={style.modal}>
+        {location === 'SHOW_LOGIN' &&
+          <LoginModal dismiss={dismiss} />}
+        {location === 'SHOW_RENT_MODAL' &&
+          <RentalItemModal dismiss={dismiss} id={itemId} />}
+      </div>
     </div>
     : null
 
 const mapStateToProps = state => ({
   visible: state.modal.visible,
-  modalContent: {
-    itemType: state.location.payload.itemType,
-    itemId: state.location.payload.itemId
-  },
+  itemId: state.location.payload.itemId,
   location: state.location.type
 })
 
--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,13 +1,27 @@
 import { createSelector } from 'reselect'
+import { getItem } from '../actions/index';
 
 const getSearchString = state => state.search.searchString
 
 const getItems = state => state.api.items
 
+const getItemById = (state, props) => {
+  const items = state.api.items
+  const filtered = items.filter(it =>
+    it._id === props.id
+  )
+  return filtered[0] || {}
+}
+
 export const getFilteredItems = createSelector(
   [getSearchString, getItems],
   (searchString, items) =>
     items.filter(item => {
-      return !searchString || item.name.toLowerCase().indexOf(searchString.toLowerCase()) >= 0
+      return !searchString || (item.available && item.name.toLowerCase().indexOf(searchString.toLowerCase()) >= 0)
     })
+)
+
+export const selectItem = createSelector(
+  [getItemById],
+  item => item
 )
